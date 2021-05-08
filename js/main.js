09f2fb8000cf5985282e1080c438b6828ec358e5
ApplexShampoox <axsxbarker@gmail.com>
2021-05-08 22:13:30 +0200

    Наполнил сайт контентом

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var swiper1 = new Swiper('.channel-slider-1', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-1',
+    prevEl: '.channel-button-prev-1',
+  },
+});
+var swiper2 = new Swiper('.channel-slider-2', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-2',
+    prevEl: '.channel-button-prev-2',
+  },
+});
+var swiper3 = new Swiper('.channel-slider-3', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-3',
+    prevEl: '.channel-button-prev-3',
+  },
+});
