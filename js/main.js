366ea889682fab3c0a8fbf316d5343c6be06822b
ApplexShampoox <axsxbarker@gmail.com>
2021-05-13 00:18:07 +0200

    Добавлен адаптивный дизайн

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 var swiper1 = new Swiper('.channel-slider-1', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next-1',
@@ -11,7 +29,16 @@ var swiper1 = new Swiper('.channel-slider-1', {
 var swiper2 = new Swiper('.channel-slider-2', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next-2',
@@ -21,10 +48,39 @@ var swiper2 = new Swiper('.channel-slider-2', {
 var swiper3 = new Swiper('.channel-slider-3', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next-3',
     prevEl: '.channel-button-prev-3',
   },
 });
+const searchBtn = document.querySelector('.moblie-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper1.destroy();
+  swiper2.destroy();
+  swiper3.destroy();
+};
